5f2533fc8f2473bd24af29585de542821b54f63c
nadunsamratunga <spyder.nadun@gmail.com>
2025-10-23 14:54:24 +0530

    Added city management function

--- main.c
+++ main.c
@@ -52,3 +52,52 @@ int main()
 
 return 0;
 }
+void cityManagement(){
+    int choice;
+    char name[50];
+    do{
+      printf("-----------CITY MANAGEMENT--------\n");
+      printf("1. Add City\n");
+      printf("2. Rename City \n");
+      printf("3. Remove City \n");
+      printf("4. List of Cities \n");
+      printf("5. Back to main menu \n");
+      printf("Enter your choice: \n");
+      scanf("%d",&choice);
+
+      if (choice==1){
+        if (cityCount<MAX_CITIES){
+            printf("Enter new City name:");
+            scanf("%s",cities[cityCount]);
+            cityCount++;
+            printf("City Saved");
+        }else printf("Max Cities Reached.\n");
+    }else if(choice==2){
+        int index;
+        printf("Enter city index to rename city\n");
+        scanf("%d",&index);
+        if(index>=0 && index<cityCount){
+            printf("Enter new name \n");
+            scanf("%s",cities[index]);
+            printf("Changed successfully\n");
+        }else printf("Invalid index!\n");
+    }else if(choice==3){
+        int i;
+        int j;
+        printf("Enter city index to remove:");
+        scanf("%d",&i);
+        if(i>=0 && index<cityCount){
+            for(j=i;j<cityCount-1;i++);
+                strcpy(cities[i],cities[i+1]);
+                cityCount--;
+                printf("City removed \n");
+        }else printf("Invalid index\n");
+    }else if(choice==4){
+        int k;
+        for (k=0:k<cityCount;k++){
+            printf("%d. %s\n",k,cities[k]);
+        }
+
+    }
+    }while(choice!=0);
+}
